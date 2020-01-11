e7112dc3d88dd452cd50a2e88400f550be1f2079
techrud <itstechrud@gmail.com>
2020-01-12 01:27:49 +0200

    added more quotes and polished it up a bit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
 const quote = [
-    'yolo for this we shall rise to the top for days to come just kidding testing long test',
-    'this is really amazing',
-    'you can make it',
-    'aweh'
+    '"Talk is cheap show me the code"',
+    '"there is no place like 127.0.0.1"',
+    '“ Code is like humor. When you have to explain it, it’s bad.”',
+    '“Make it work, make it right, make it fast.” – Kent Beck',
+    '“Ruby is rubbish! PHP is phpantastic!” – Nikita Popov'
 ];
-
 
 document.getElementById("quote").textContent = quote[Math.floor(Math.random() * quote.length)];
